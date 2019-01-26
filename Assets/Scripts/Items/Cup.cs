0984f4a26093f0a3e2212943e2d667fb9b87dc79
Ivan Notaros <nothke@gmail.com>
2019-01-26 21:11:31 +0100

    Bugfixes

--- Assets/Scripts/Items/Cup.cs
+++ Assets/Scripts/Items/Cup.cs
@@ -7,8 +7,39 @@ public class Cup : Item
     public Drink drinkType;
     public float amount;
 
+    Slavina slavina;
+
     private void Update()
     {
         DebugUtils.Meter(amount, transform.position, 0.2f, DrinkFoodUtils.GetColor(drinkType));
+
+        if (slavina)
+        {
+            amount += Time.deltaTime * slavina.fillSpeed;
+        }
+    }
+
+    protected override void OnPlacedInSlot(Slot slot)
+    {
+        if (slot is Slavina)
+        {
+            slavina = slot as Slavina;
+            slavina.PourWater();
+
+            if (drinkType != Drink.Water)
+            {
+                amount = 0;
+                drinkType = Drink.Water;
+            }
+        }
+    }
+
+    protected override void OnRemovedFromSlot(Slot slot)
+    {
+        if (slavina)
+        {
+            slavina.EndPouringWater();
+            slavina = null;
+        }
     }
 }
