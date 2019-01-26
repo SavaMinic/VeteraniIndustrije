78e8f04a3060ab2fc6e2ca053b62a1eea1141e53
Sava Minic <savam@nordeus.com>
2019-01-26 15:21:37 +0100

    sample scene

---
+++ Assets/Scripts/MainPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+    
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void FixedUpdate ()
+    {
+        if (!Application.isPlaying)
+            return;
+        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Debug.LogError(moveHorizontal + " " + moveVertical);
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
+}
