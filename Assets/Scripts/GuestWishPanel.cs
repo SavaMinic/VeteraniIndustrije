fa1c02395de8e878eeb03b55225fa6ebd6323220
Sava Minic <savam@nordeus.com>
2019-01-26 19:43:55 +0100

    base ui for guest wishes

---
+++ Assets/Scripts/GuestWishPanel.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuestWishPanel : MonoBehaviour
+{
+    #region Properties/Fields
+
+    [SerializeField]
+    private Vector3 offset;
+
+    [SerializeField]
+    private float fadeDuration;
+
+    [SerializeField]
+    private Image wishBackground;
+
+    [SerializeField]
+    private Image wishImage;
+
+    private CanvasGroup canvasGroup;
+
+    private Guest guest;
+
+    private bool isShown;
+
+    #endregion
+
+    #region Mono
+
+    private void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        isShown = false;
+        canvasGroup.alpha = 0f;
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+
+    private void Update()
+    {
+        if (!Application.isPlaying || guest == null)
+            return;
+
+        if (guest == null)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+        
+        // update position
+        var worldPos = guest.transform.position + offset;
+        transform.position = Util.WorldToUISpace(CanvasController.I.MainCanvas, worldPos);;
+        
+        var guestState = guest.CurrentState;
+        if (guestState == Guest.GuestState.WaitingForService && !isShown)
+        {
+            isShown = true;
+            StartCoroutine(FadeTo(1f, fadeDuration));
+        }
+        else if (guestState != Guest.GuestState.WaitingForService && isShown)
+        {
+            isShown = false;
+            StartCoroutine(FadeTo(0f, fadeDuration));
+        }
+    }
+
+    #endregion
+
+    #region Public
+
+    public void SetGuest(Guest myGuest)
+    {
+        guest = myGuest;
+        gameObject.SetActive(true);
+    }
+
+    #endregion
+
+    #region Private
+
+    IEnumerator FadeTo(float aValue, float aTime)
+    {
+        float alpha = canvasGroup.alpha;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+        {
+            canvasGroup.alpha = Mathf.Lerp(alpha,aValue,t);
+            yield return null;
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Util.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Util
+{
+    public static Vector3 WorldToUISpace(Canvas parentCanvas, Vector3 worldPos)
+    {
+        //Convert the world for screen point so that it can be used with ScreenPointToLocalPointInRectangle function
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(worldPos);
+        Vector2 movePos;
+
+        //Convert the screenpoint to ui rectangle local point
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(parentCanvas.transform as RectTransform, screenPos, parentCanvas.worldCamera, out movePos);
+        //Convert the local point to world point
+        return parentCanvas.transform.TransformPoint(movePos);
+    }
+}
