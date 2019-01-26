e6c4c57fd60a5a21b4b3e51e6d960a5af11339aa
Sava Minic <savam@nordeus.com>
2019-01-26 15:27:49 +0100

    updated movement

--- Assets/Scripts/MainPlayer.cs
+++ Assets/Scripts/MainPlayer.cs
@@ -18,9 +18,8 @@ public class MainPlayer : MonoBehaviour
         if (!Application.isPlaying)
             return;
         
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Debug.LogError(moveHorizontal + " " + moveVertical);
+        float moveHorizontal = -Input.GetAxis("Horizontal");
+        float moveVertical = -Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
