7820ac202f32606a6b750386c82d255ef63bec92
Ivan Notaros <nothke@gmail.com>
2019-01-27 17:00:58 +0100

    Zvuk!!

--- Assets/Scripts/Items/Ringla.cs
+++ Assets/Scripts/Items/Ringla.cs
@@ -5,4 +5,11 @@ using UnityEngine;
 public class Ringla : Slot
 {
     public bool isOn;
+
+    public AudioSource vriSource;
+
+    private void Update()
+    {
+        vriSource.enabled = itemInSlot == true;
+    }
 }
