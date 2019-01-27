45b182d077f9ea2a673ce436c7fe657d9c588405
Sava Minic <savam@nordeus.com>
2019-01-27 16:15:12 +0100

    slava

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -60,7 +60,7 @@ public class CanvasController : MonoBehaviour
     public void AddStars(int stars)
     {
         starsCount += stars;
-        starsCountText.text = "STARS: " + starsCount;
+        starsCountText.text = "SLAVA: " + starsCount;
     }
     
     public void AddNewGuestWish(Guest guest)
