24102c1261449137fa018a0adb5b89e7a86fd1d6
Ivan Notaros <nothke@gmail.com>
2019-01-27 16:19:15 +0100

    Casa can be filled

--- Assets/Scripts/Items/Bottle.cs
+++ Assets/Scripts/Items/Bottle.cs
@@ -13,9 +13,28 @@ public class Bottle : Item//, IDrinkContainer
 {
     public DrinkContainer container;
 
+    Slavina slavina;
+
     private void OnDrawGizmos()
     {
         if (container)
             DebugUtils.Meter(container.amount / container.maxAmount, transform.position, 0.2f, DrinkFoodUtils.GetColor(container.drinkType));
+    }
+
+    protected override void OnPlacedInSlot(Slot slot)
+    {
+        if (slot is Slavina)
+        {
+            slavina = slot as Slavina;
+            slavina.ShowMlaz();
+        }
+    }
+
+    protected override void OnRemovedFromSlot(Slot slot)
+    {
+        if (slot is Slavina)
+        {
+            slavina.EndPouringWater();
+        }
     }
 }
