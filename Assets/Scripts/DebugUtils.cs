6a78eea7096bc2b6020bc09082c756bf2a6ceb0b
Ivan Notaros <nothke@gmail.com>
2019-01-27 02:46:48 +0100

    Increased debug lines scale

--- Assets/Scripts/DebugUtils.cs
+++ Assets/Scripts/DebugUtils.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public static class DebugUtils
 {
+    const float scale = 5;
+
     public static void Meter(float value, Vector3 position, float offset, Color color)
     {
-        Debug.DrawRay(position + Vector3.right * offset, Vector3.up, Color.gray);
-        Debug.DrawRay(position + Vector3.right * offset, Vector3.up * value, color);
+        Debug.DrawRay(position + Vector3.right * offset * scale, Vector3.up * scale, Color.gray);
+        Debug.DrawRay(position + Vector3.right * offset * scale, Vector3.up * value * scale, color);
     }
 }
