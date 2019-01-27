2949d3d75a395f5a4773269920c0bbe5e0a9f927
Ivan Notaros <nothke@gmail.com>
2019-01-27 01:57:34 +0100

    Changed player motion to be camera relative to prevent unequal horizontal and vertical forces

--- Assets/Scripts/MainPlayer.cs
+++ Assets/Scripts/MainPlayer.cs
@@ -12,19 +12,19 @@ public class MainPlayer : MonoBehaviour
     private Rigidbody rb;
     private SpriteRenderer spriteRenderer;
     private Animator animator;
-    
-    void Start ()
+
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
-    
-    void FixedUpdate ()
+
+    void FixedUpdate()
     {
         if (!Application.isPlaying)
             return;
-        
+
         float h = Input.GetAxis(HorizontalAxis);
         float v = Input.GetAxis(VerticalAxis);
 
@@ -34,10 +34,21 @@ public class MainPlayer : MonoBehaviour
         }
         animator.SetBool("walking", Mathf.Abs(h) > 0 || Mathf.Abs(v) > 0);
 
-        // since camera is rotated, recalculate the movement so x/z axis are appropriate for camera
-        // minus is there because camera is looking back
-        Vector3 movement =  -new Vector3(v + h, 0.0f, v - h);
+        // Move player relative to camera, but projected to ground
+        Vector3 inputV = Vector3.ClampMagnitude(new Vector3(h, v), 1);
+        Transform camT = Camera.main.transform;
+        Vector3 camRelativeV = camT.right * inputV.x + camT.up * inputV.y;
 
-        rb.AddForce (movement * speed);
+        Vector3 targetPosition = transform.position + camRelativeV;
+        targetPosition.y = 0;
+
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        Ray ray = new Ray(camRelativeV, camT.forward);
+
+        float enter;
+        groundPlane.Raycast(ray, out enter);
+        Vector3 forceV = ray.GetPoint(enter) * speed;
+
+        rb.AddForce(forceV);
     }
 }
