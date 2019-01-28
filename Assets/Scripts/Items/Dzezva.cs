40986f0210e825d005d900b897f5164eb03debd5
Ivan Notaros <nothke@gmail.com>
2019-01-28 18:32:02 +0100

    Removed kipi-ing and cooling from dzezva as it is handled by container now

--- Assets/Scripts/Items/Dzezva.cs
+++ Assets/Scripts/Items/Dzezva.cs
@@ -7,35 +7,17 @@ public class Dzezva : Interactable
     public DrinkContainer container;
     public bool hasCoffee;
 
-    public float kipi;
-
     public float heatingSpeed = 0.1f;
-    public float kipiSpeed = 0.1f;
-    public float coolingSpeed = 0.01f;
 
     public Ringla ringla;
 
     private void Update()
     {
         if (ringla && ringla.isOn && container.amount >= 1)
-        {
-            container.temperature += Time.deltaTime * heatingSpeed;
-
-            if (container.temperature >= 1)
-                kipi += Time.deltaTime * kipiSpeed;
-        }
-        else
-        {
-            container.temperature -= Time.deltaTime * coolingSpeed;
-            kipi -= Time.deltaTime * kipiSpeed;
-        }
-
-        container.temperature = Mathf.Clamp01(container.temperature);
-        kipi = Mathf.Clamp01(kipi);
+            container.AddHeat(Time.deltaTime * heatingSpeed);
 
         // Debug lines
         DebugUtils.Meter(container.temperature, transform.position, 0.2f, Color.red);
-        DebugUtils.Meter(kipi, transform.position, 0.3f, Color.yellow);
         DebugUtils.Meter(container.amount, transform.position, 0.4f, DrinkFoodUtils.GetColor(container.drinkType));
     }
 
