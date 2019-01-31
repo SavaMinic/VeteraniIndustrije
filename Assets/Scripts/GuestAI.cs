f4d007fb5423819b926e250256c93e2b4ff025e2
Ivan Notaros <nothke@gmail.com>
2019-01-31 15:19:38 +0100

    AI pathfinding

---
+++ Assets/Scripts/GuestAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+
+public class GuestAI : MonoBehaviour
+{
+    NavMeshAgent _agent;
+    NavMeshAgent agent { get { if (!_agent) _agent = GetComponent<NavMeshAgent>(); return _agent; } }
+
+    public bool ReachedDestination => agent.isStopped;
+
+    public Transform exitDestination;
+    public Transform zitoDestination;
+    public Transform seatDestination;
+
+    void Start()
+    {
+        GotoSeat();
+    }
+
+    void GotoSeat()
+    {
+        agent.SetDestination(seatDestination.position);
+    }
+
+    void Update()
+    {
+        Debug.DrawLine(transform.position, agent.destination);
+        Debug.DrawRay(transform.position, Vector3.up, ReachedDestination ? Color.yellow : Color.blue);
+    }
+}
