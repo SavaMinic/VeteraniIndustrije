e69bac055c8d469fdad3f5b2ebb0b0d5a5dc7e3d
Ivan Notaros <nothke@gmail.com>
2019-02-02 21:31:01 +0100

    Bell

---
+++ Assets/Scripts/Bell.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bell : MonoBehaviour
+{
+    public static Bell e;
+    void Awake() { e = this; }
+
+    [SerializeField] AudioSource ring;
+
+    public float ringingTime;
+
+    bool isRinging;
+
+    [SerializeField] GameObject ringingSprite;
+    [SerializeField] GameObject staticSprite;
+
+    public void Ring()
+    {
+        if (isRinging) return;
+
+        isRinging = true;
+
+        ringingSprite.SetActive(true);
+        staticSprite.SetActive(false);
+
+        ring.Play();
+        StartCoroutine(EndRing());
+    }
+
+    IEnumerator EndRing()
+    {
+        yield return new WaitForSeconds(2);
+
+        ringingSprite.SetActive(false);
+        staticSprite.SetActive(true);
+
+        isRinging = false;
+    }
+
+}
---
+++ Assets/Scripts/RingTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingTest : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Attemping to ring");
+            Bell.e.Ring();
+
+        }
+    }
+}
