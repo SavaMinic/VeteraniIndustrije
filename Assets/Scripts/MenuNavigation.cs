645b8e84ce758d208cdee6fe64c24cbe6008308a
Ivan Notaros <nothke@gmail.com>
2019-12-19 01:41:08 +0100

    Main menu and sprite updates

---
+++ Assets/Scripts/MenuNavigation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+
+public class MenuNavigation : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+}
