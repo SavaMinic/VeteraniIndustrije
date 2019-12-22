d68068851f0cb2a181545ecc5456862145fd4f67
Ivan Notaros <nothke@gmail.com>
2019-12-22 04:58:55 +0100

    Smederevac now opens the lid when empty

--- Assets/Scripts/Items/Rerna.cs
+++ Assets/Scripts/Items/Rerna.cs
@@ -6,15 +6,35 @@ public class Rerna : Slot
 {
     public float heatingSpeed = 0.1f;
 
+    public SpriteRenderer closedSprite;
+    public SpriteRenderer overlaySprite;
+    public SpriteRenderer openSprite;
+
     private void Update()
     {
         if (!Application.isPlaying || GameController.I.IsPaused)
             return;
-        
+
         if (itemInSlot is LonacSarme)
         {
             Container container = itemInSlot.GetComponent<Container>();
             container.AddHeat(Time.deltaTime * heatingSpeed);
         }
+    }
+
+    public override void OnItemRemoved()
+    {
+        closedSprite.enabled = false;
+        overlaySprite.enabled = false;
+
+        openSprite.enabled = true;
+    }
+
+    public override void OnItemPlaced()
+    {
+        closedSprite.enabled = true;
+        overlaySprite.enabled = true;
+
+        openSprite.enabled = false;
     }
 }
