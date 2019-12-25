c275cd79ff8e481f31c8f6b8a93f619b27e006de
Ivan Notaros <nothke@gmail.com>
2019-12-25 02:29:07 +0100

    Copied level data to level assets

---
+++ Assets/Scripts/LevelAsset.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "LevelAsset")]
+public class LevelAsset : ScriptableObject
+{
+    public int CandleDuration;
+    public int LimitGuestNumber;
+    public List<GuestWish.GuestWishType> AvailableWishes;
+    public bool IsRaining;
+}
